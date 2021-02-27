d24a296dea00d1606289d410649e0241a45f978f
AquaXV <aquaxv@jimune.com>
2021-02-27 14:51:19 +0100

    Initial

--- src/main/code/bot/commands/cmd_ping.ts
+++
@@ -1,9 +0,0 @@
-import CommandContext from "./context";
-
-module.exports = {
-    name: 'ping',
-    description: 'Ping Pong!',
-    execute(context: CommandContext, args: string[]) {
-        context.message.channel.send('Pong!');
-    }
-}
